876445b55e30032851b3ec2ff73d56a3dfc653ca
realowner <iamrealowner@gmail.com>
2021-06-21 13:48:06 +0300

    socketio fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,10 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-# from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config.from_object(Config)
 database = SQLAlchemy(app)
 migrate = Migrate(app, database)
-# socketio = SocketIO(app=app)
 
 from app import routes
