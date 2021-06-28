3a458c2ea3a46181feba192afda8dde1f304e242
realowner <iamrealowner@gmail.com>
2021-06-28 11:30:17 +0300

    empty db

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db') + '?check_same_thread=False'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test_database.db') + '?check_same_thread=False'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db') + '?check_same_thread=False'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test_database.db') + '?check_same_thread=False'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
