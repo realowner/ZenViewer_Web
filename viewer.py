b76ee99a69e7ec9c23d93ed7c2f77d262879b2db
realowner <iamrealowner@gmail.com>
2021-07-26 14:13:53 +0300

    headless

--- viewer.py
+++ viewer.py
@@ -10,22 +10,22 @@ class MyDaemon(Daemon):
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3729, debug=True)
+    # app.run(host='localhost', port=3729, debug=True)
 
-    # daemon = MyDaemon(f'{basedir}/daemon-zenviewer.pid')
-    # if len(sys.argv) == 2:
-    #     if 'start' == sys.argv[1]:
-    #         daemon.start()
-    #     elif 'stop' == sys.argv[1]:
-    #         daemon.stop()
-    #     elif 'restart' == sys.argv[1]:
-    #         daemon.restart()
-    #     elif 'status' == sys.argv[1]:
-    #         daemon.status()
-    #     else:
-    #         print("Unknown command")
-    #         sys.exit(2)
-    #     sys.exit(0)
-    # else:
-    #     print("usage: %s start|stop|restart" % sys.argv[0])
-    #     sys.exit(2)
+    daemon = MyDaemon(f'{basedir}/daemon-zenviewer.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        elif 'status' == sys.argv[1]:
+            daemon.status()
+        else:
+            print("Unknown command")
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print("usage: %s start|stop|restart" % sys.argv[0])
+        sys.exit(2)
